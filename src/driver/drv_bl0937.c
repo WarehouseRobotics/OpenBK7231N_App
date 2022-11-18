b171aa3c2431d29fc493e03974b1e5e6559b0424
murinson <urinson@gmail.com>
2022-11-18 16:24:22 +0100

    fixed query string for get request

--- src/driver/drv_bl0937.c
+++ src/driver/drv_bl0937.c
@@ -296,7 +296,7 @@ void BL0937_RunFrame()
 	BL_ProcessUpdate(final_v, final_c, final_p);
 
 	char url[256];
-	snprintf(url, sizeof(url), "https://webhook.site/9fae089d-ef93-4fa4-a472-092e78b9e164?Voltage=%.2f,Current=%.2f,Power=%.2f,UpTimeSec=%d",
+	snprintf(url, sizeof(url), "https://webhook.site/9fae089d-ef93-4fa4-a472-092e78b9e164?Voltage=%.2f&Current=%.2f&Power=%.2f&UpTimeSec=%d",
 		final_v, final_c, final_p, Time_getUpTimeSeconds());
 
 	HTTPClient_Async_SendGetWithAuth(url, "testuser", "testpass");
